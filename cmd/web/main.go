4d24990130b6e882f81c2de5bce210eb20646d93
Med-IDBENOUAKRIM <medidbenouakrim@gmail.com>
2024-12-31 14:21:48 +0100

    add env file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
 type Application struct {
@@ -21,7 +26,39 @@ func main() {
 
 	log.Println("starting server on :4000")
 
-	err := http.ListenAndServe(":4000", app.routes())
+	db, err := connectDB()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	logger.Info("db connection is established")
+
+	err = http.ListenAndServe(":4000", app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func connectDB() (*sql.DB, error) {
+	dbSource := os.Getenv("DB_SOURCE")
+
+	db, err := sql.Open("postgres", dbSource)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+
 }
